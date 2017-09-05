cd6517aa96072d367a9bc359e388e49d23a48d26
Anas Rchid <rchid.anas@gmail.com>
2017-09-05 18:29:13 +0100

    solving towers of hanoi..

---
+++ problems/towers-of-hanoi.cc
@@ -0,0 +1,22 @@
+// Tower of Hanoi (https://en.wikipedia.org/wiki/Tower_of_Hanoi)
+//
+// The Towers of Hanoi puzzle begins with three poles and n rings,
+// where all rings start on the leftmost pole (labeled Pole 1).
+// The rings each have a different size, and are stacked in order
+// of decreasing size with the largest ring at the bottom.
+// The problem is to move the rings from the leftmost pole to the
+// rightmost pole (labeled Pole 3) in a series of steps. At each
+// step the top ring on some pole is moved to another pole. There
+// is one limitation on where rings may be moved: A ring can never
+// be moved on top of a smaller ring.
+
+#include <iostream>
+
+#define N_TOWERS	(3)
+#define N_RINGS		(7)
+#define RINGS_DIFF	(1)
+
+int main(int argc, char **argv) {
+
+  return 0;
+}
